4462ee63f525db09164a48623af7a564536104ea
Jonathan Hainsworth <jonathanhainsworth@yahoo.co.uk>
2022-10-15 14:59:20 +0100

    DI, Service Test Setup

--- AverageTemperatureApplication/Program.cs
+++ AverageTemperatureApplication/Program.cs
@@ -1,6 +1,16 @@
+using AverageTemperatureApplication.DAL.Impementations;
+using AverageTemperatureApplication.DAL.Interfaces;
+using AverageTemperatureApplication.Services.Implementations;
+using AverageTemperatureApplication.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddTransient<IAverageTemperateCache, AverageTemperateCache>();
+builder.Services.AddTransient<IAverageTemperatureSourceA, AverageTemperatureSourceA>();
+builder.Services.AddTransient<IAverageTemperatureSourceB, AverageTemperatureSourceB>();
+
+builder.Services.AddScoped<IAverageTemperatureService, AverageTemperatureService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- AverageTemperatureApplication/Services/Implementations/AverageTemperatureService.cs
+++ AverageTemperatureApplication/Services/Implementations/AverageTemperatureService.cs
@@ -1,10 +1,25 @@
-﻿using AverageTemperatureApplication.Services.Interfaces;
+﻿using AverageTemperatureApplication.DAL.Interfaces;
+using AverageTemperatureApplication.Services.Interfaces;
 using Models;
 
 namespace AverageTemperatureApplication.Services.Implementations
 {
     public class AverageTemperatureService : IAverageTemperatureService
     {
+        private IAverageTemperateCache _averageTemperateCache;
+        private IAverageTemperatureSourceA _averageTemperatureSourceA;
+        private IAverageTemperatureSourceB _averageTemperatureSourceB;
+
+        public AverageTemperatureService(
+            IAverageTemperateCache averageTemperateCache,
+            IAverageTemperatureSourceA averageTemperatureSourceA,
+            IAverageTemperatureSourceB averageTemperatureSourceB)
+        {
+            _averageTemperateCache = averageTemperateCache;
+            _averageTemperatureSourceA = averageTemperatureSourceA;
+            _averageTemperatureSourceB = averageTemperatureSourceB;
+        }
+
         public AverageTemperature getAverageTemperature()
         {
             throw new NotImplementedException();
