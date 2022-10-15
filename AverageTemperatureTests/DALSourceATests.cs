917ad2205f696573c63f4d015457637f4609b864
Jonathan Hainsworth <jonathanhainsworth@yahoo.co.uk>
2022-10-15 22:49:04 +0100

    DAL tests, mocks currently unavalible

---
+++ AverageTemperatureTests/DALSourceATests.cs
@@ -0,0 +1,63 @@
+﻿using AverageTemperatureApplication.DAL.Impementations;
+using Models;
+using NUnit.Framework;
+using System;
+using System.Diagnostics;
+
+namespace AverageTemperatureTests
+{
+    public class DALSourceATests
+    {
+        [Test]
+        public void SuccesfullyGetDataFromSourceA()
+        {
+            // Arrange
+            double latitude = 55.9486;
+            double longitude = 3.1999;
+            DateOnly endDate = new DateOnly(2022, 10, 15);
+
+            var DalResponse = new AverageTemperature
+            {
+                Temperature = 5.5,
+                Latitude = latitude,
+                Longitude = longitude,
+                StartDay = DateOnly.FromDateTime(new DateTime()),
+                EndDay = DateOnly.FromDateTime(new DateTime().AddDays(-5)),
+                isSuccessfull = true,
+                isRequestLimitReached = false
+            };
+
+            // Assert
+            var result = new AverageTemperatureSourceA().GetAverageTemperature(latitude, longitude, endDate);
+
+            // Act
+            Debug.Assert(false);
+        }
+
+        [Test]
+        public void UnsuccesfullyGetDataFromSourceA()
+        {
+            // Arrange
+            double latitude = 55.9486;
+            double longitude = 3.1999;
+            DateOnly endDate = new DateOnly(2022, 10, 15);
+
+            var DalResponse = new AverageTemperature
+            {
+                Temperature = 5.5,
+                Latitude = latitude,
+                Longitude = longitude,
+                StartDay = DateOnly.FromDateTime(new DateTime()),
+                EndDay = DateOnly.FromDateTime(new DateTime().AddDays(-5)),
+                isSuccessfull = false,
+                isRequestLimitReached = false
+            };
+
+            // Assert
+            var result = new AverageTemperatureSourceA().GetAverageTemperature(latitude, longitude, endDate);
+
+            // Act
+            Debug.Assert(false);
+        }
+    }
+}
